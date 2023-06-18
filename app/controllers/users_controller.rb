1aac147febfde35294b8ecec62eb34a0a9e76572
bondinu <spymissionwakuwaku06@gmail.com>
2023-06-18 18:28:43 +0900

    ユーザー詳細ページ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
-    @users = User.find(params[:id])
+    @user = User.find(params[:id])
+    @prototypes = Prototype.includes(:user)
   end
 end
